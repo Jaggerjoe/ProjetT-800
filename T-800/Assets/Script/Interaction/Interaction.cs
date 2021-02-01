114735e38fa318d7adf726d64a9b1d1258263368
JaggerJoe <gregoire.thomassin@e-artsup.net>
2021-02-01 18:46:07 +0100

    Interaction

---
+++ T-800/Assets/Script/Interaction/Interaction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_Target;
+
+    [SerializeField]
+    private LayerMask m_Layer;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void ActionLevier()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward, Color.cyan);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10, m_Layer))
+        {
+            Debug.Log("je suis touché");
+        }
+    }
+}
