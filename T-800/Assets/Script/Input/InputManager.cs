8a5e0cff01c05ad224a1922c4f5923ff69ff6289
JaggerJoe <gregoire.thomassin@e-artsup.net>
2021-02-05 17:26:40 +0100

    Push dplc et jupmp

--- T-800/Assets/Script/Input/InputManager.cs
+++ T-800/Assets/Script/Input/InputManager.cs
@@ -8,36 +8,43 @@ public class InputManager : MonoBehaviour
     [SerializeField]
     private InputActionAsset m_InputManage = null;
 
+    [SerializeField]
     private Movements m_Movements = null;
 
+    [SerializeField]
     private Interaction m_Interaction = null;
 
-
+    [SerializeField]
+    private TPSScript m_RefCamera = null;
 
     Vector2 m_PosCamera = Vector2.zero;
 
+    Vector2 m_Movement = Vector2.zero;
+
     private void Awake()
     {
-        m_Movements = FindObjectOfType<Movements>();
-        m_Interaction = FindObjectOfType<Interaction>();
-
         InputActionMap playerMap = m_InputManage.FindActionMap("Player");
+
         InputAction rotationCamera = m_InputManage.FindAction("Look");
         rotationCamera.performed += (ctx) => { m_PosCamera = ctx.ReadValue<Vector2>(); };
         rotationCamera.canceled += (ctx) => { m_PosCamera = Vector2.zero; };
 
         InputAction moveAction = playerMap.FindAction("Movements");
-        moveAction.performed += (ctx) => { m_Movements.movementInput = ctx.ReadValue<Vector2>();  };
-        moveAction.canceled += (ctx) => { m_Movements.movementInput = Vector2.zero; };
+        moveAction.performed += (ctx) => { m_Movement = ctx.ReadValue<Vector2>(); };
+        moveAction.canceled += (ctx) => { m_Movement = Vector2.zero; };
 
         InputAction interactinAction = playerMap.FindAction("Interaction");
-        interactinAction.started += (ctx) => m_Interaction.ActionLevier();
+        interactinAction.started += (ctx) => m_Interaction.Action();
+
+        InputAction jumpAction = playerMap.FindAction("Jump");
+        jumpAction.performed += (ctx) => { m_Movements.Jump(); };
+        //jumpAction.canceled += (ctx) => {  };
     }
 
     private void Update()
     {
-        m_Movements.Move(m_Movements.movementInput, Time.deltaTime);
-
+        m_Movements.Move(m_Movement, Time.deltaTime);
+        m_RefCamera.RotationCamera(m_PosCamera);
     }
     private void OnEnable()
     {
