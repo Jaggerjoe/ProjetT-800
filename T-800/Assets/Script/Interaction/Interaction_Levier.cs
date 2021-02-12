996736c3ac6b66e78a121deb00a5d0af1e06b189
JaggerJoe <gregoire.thomassin@e-artsup.net>
2021-02-12 18:12:10 +0100

    Jump
    ModificationJump, just jump one time

--- T-800/Assets/Script/Interaction/Interaction_Levier.cs
+++ T-800/Assets/Script/Interaction/Interaction_Levier.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IT_Levier : MonoBehaviour
+public class Interaction_Levier : InteractionMother
 {
     [SerializeField]
     private GameObject m_Levier;
     private Animator m_Anim;
     private void Start()
     {
-        m_Anim = GetComponent<Animator>();
+ 
     }
     // Start is called before the first frame update
 
+    public override void Use()
+    {
+        if(TryGetComponent(out m_Anim))
+        {
+            m_Anim.SetTrigger("Interact");
+        }
+        else
+        {
+            Debug.Log("je suis un levier mais sans mon aniamtion");
+            return;
+        }
+    }
     public void Levier()
     {
         m_Anim.SetTrigger("Interact");
